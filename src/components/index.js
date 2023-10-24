53c27d1ad5b53b3ee6df19094c905846bf19d2e5
Ekaterina Kryuchkova <krekser37@yandex.ru>
2023-10-24 19:09:46 +0300

    feat/ add expert_mobile

--- src/components/index.js
+++ src/components/index.js
@@ -6,14 +6,10 @@ export const forecastButtonDown = document.querySelector('.forecast_down');
 export const forecastDescription = document.querySelector(
   '.forecast_description'
 );
-
-export const kefCoefName = document.querySelector(
-  '.kef_coef_name'
-);
-export const expertTem = document.querySelector('.expert_term');
 export const forecastShadow = document.querySelector('.forecast_shadow');
 
-
+export const kefCoefName = document.querySelector('.kef_coef_name');
+export const expertTem = document.querySelector('.expert_term');
 
 forecastButton.addEventListener('click', function () {
   const heightBlock = getComputedStyle(forecastDescription).height;
@@ -34,11 +30,11 @@ forecastButton.addEventListener('click', function () {
 
 window.addEventListener('resize', function () {
   const widthBlock = document.body.clientWidth;
-  widthBlock < '744'
-    ? (expertTem.textContent = '30 дней')
-    : (expertTem.textContent = 'Статистика за последние 30 дней');
-    widthBlock < '744'
-    ? (kefCoefName.textContent = 'Кэф')
-    : (kefCoefName.textContent = 'Коэффициент');
-
+  if (widthBlock < '744') {
+    expertTem.textContent = '30 дней';
+    kefCoefName.textContent = 'Кэф';
+  } else {
+    expertTem.textContent = 'Статистика за последние 30 дней';
+    kefCoefName.textContent = 'Коэффициент';
+  }
 });
